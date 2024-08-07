aec259f0f0924d79746bbc7041d0518e96b28dff
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-07 13:45:30 -0400

    debugged select sort to provide proper output

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 
     for (i = 0; i < size - 1; i++)
     {
-        // Find the index of the minimum element in the unsorted portion
+        /* Find the index of the minimum element in the unsorted portion */
         min_idx = i;
         for (j = i + 1; j < size; j++)
         {
@@ -23,14 +23,14 @@ void selection_sort(int *array, size_t size)
                 min_idx = j;
         }
 
-        // If min_idx is not the position of the current element, swap
+        /* If min_idx is not the position of the current element, swap */
         if (min_idx != i)
         {
             temp = array[i];
             array[i] = array[min_idx];
             array[min_idx] = temp;
 
-            // Print the array after the swap
+            /* Print the array after the swap */
             print_array(array, size);
         }
     }
