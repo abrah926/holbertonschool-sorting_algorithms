dc703a715ed07b3616268b092a24ee87ba84b1af
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-07 13:01:52 -0400

    func that sorts array of ints in ascending order usint selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = 1;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+
+			print_array(array, size);
+		}
+	}
+}
