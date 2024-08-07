731d7c7724b1433ab5d68f66300ac944acbec491
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-07 13:36:49 -0400

    debugged select sort to provide proper output

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,38 @@
-#include <stdio.h>
 #include "sort.h"
 
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * @array: The array to sort
+ * @size: The size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-	int temp;
+    size_t i, j, min_idx;
+    int temp;
 
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		min_index = 1;
+    for (i = 0; i < size - 1; i++)
+    {
+        // Find the index of the minimum element in the unsorted portion
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
 
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_index])
-				min_index = j;
-		}
+        // If min_idx is not the position of the current element, swap
+        if (min_idx != i)
+        {
+            temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
 
-		if (min_index != i)
-		{
-			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
-
-			print_array(array, size);
-		}
-	}
+            // Print the array after the swap
+            print_array(array, size);
+        }
+    }
 }
+
