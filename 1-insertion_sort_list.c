d0c3a588f99f84b4d63d2bcf509f6f21492fa4e8
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-06 21:07:20 -0400

    func that sorts a doubly linked list in ascension with insert sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list.
+ * @list: Pointer to the head of the list.
+ * @node1: The first node to swap.
+ * @node2: The second node to swap.
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	node1->prev = node2;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm.
+ * @list: Pointer to the head of the doubly linked list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *next, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		next = current->next;
+		while (current->prev && current->prev->n > current->n)
+		{
+			temp = current->prev;
+			swap_nodes(list, temp, current);
+			print_list(*list);
+		}
+		current = next;
+	}
+}
